83184bd8ac64f419774881c4c369f0bca92cc76a
Conrad Schmidt <246308dino@gmail.com>
2026-02-11 03:28:52 +0000

    File changes

--- src/pages/RoomDesigner.jsx
+++ src/pages/RoomDesigner.jsx
@@ -747,18 +747,6 @@ Style: Photorealistic 3D render, ${formData.event_type ? formData.event_type.rep
                       {showGearList ? 'Hide' : 'Show'} Gear List
                     </Button>
                   </div>
-                  <Button
-                    onClick={handleGetAISuggestions}
-                    disabled={aiLoading}
-                    className="w-full bg-gradient-to-r from-blue-500 to-cyan-500 hover:from-blue-600 hover:to-cyan-600 text-white font-semibold h-12 rounded-lg"
-                  >
-                    {aiLoading ? (
-                      <Loader2 className="w-5 h-5 animate-spin mr-2" />
-                    ) : (
-                      <Lightbulb className="w-5 h-5 mr-2" />
-                    )}
-                    Get AI Suggestions
-                  </Button>
                 </div>
               </div>
             ) : (
